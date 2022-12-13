fe031f2f0af2b60fcdb0281decf868db1ed35b61
blourenco217 <beatriz.b.lourenco@tecnico.ulisboa.pt>
2022-12-13 23:08:21 +0000

    theta threshold applied and good features consideration when sampling
    updates feitos em segmentation.py para nao interferir com restantes files

--- line_segmentation.py
+++ line_segmentation.py
@@ -3,27 +3,39 @@ import numpy as np
 import cv2
 from scipy.spatial.distance import cdist
 
-def angle(dir):
+import math
+
+def angle(x1, y1, x2, y2):
     """
-    Returns the angles between vectors.
-
-    Parameters:
-    dir is a 2D-array of shape (N,M) representing N vectors in M-dimensional space.
-
     The return value is a 1D-array of values of shape (N-1,), with each value
     between 0 and pi.
-
-    0 implies the vectors point in the same direction
-    pi/2 implies the vectors are orthogonal
-    pi implies the vectors point in opposite directions
+    0 implies points in the same direction
+    pi/2 implies points are orthogonal
+    pi implies points in opposite directions
     """
-    dir2 = dir[1:]
-    dir1 = dir[:-1]
-    return np.arccos((dir1*dir2).sum(axis=1)/(
-        np.sqrt((dir1**2).sum(axis=1)*(dir2**2).sum(axis=1))))
+    return math.atan2(y2-y1, x2-x1)
+
+# def angle(dir):
+#     """
+#     Returns the angles between vectors.
+
+#     Parameters:
+#     dir is a 2D-array of shape (N,M) representing N vectors in M-dimensional space.
+
+#     The return value is a 1D-array of values of shape (N-1,), with each value
+#     between 0 and pi.
+
+#     0 implies the vectors point in the same direction
+#     pi/2 implies the vectors are orthogonal
+#     pi implies the vectors point in opposite directions
+#     """
+#     dir2 = dir[1:]
+#     dir1 = dir[:-1]
+#     return np.arccos((dir1*dir2).sum(axis=1)/(
+#         np.sqrt((dir1**2).sum(axis=1)*(dir2**2).sum(axis=1))))
 
 def sampling(x, y):
-    return x[::100], y[::100]
+    return x[::300], y[::300]
 
 def find_nearest(array, value):
     # array = np.asarray(array)
@@ -57,15 +69,20 @@ def get_end_pnts(points, image):
 def order_points(points, ind):
     points_new = [ points.pop(ind) ]  # initialize a new list of points with the known first point
     pcurr      = points_new[-1]       # initialize the current point (as the known point)
+    print(len(points))
     while len(points)>0:
         d      = np.linalg.norm(np.array(points) - np.array(pcurr), axis=1)  # distances between pcurr and all other remaining points
         ind    = d.argmin()                   # index of the closest point
         if d[ind] > 500:
+            print(pcurr)
+            print(points)
+            print(len(points))
             print('jump')
             np.concatenate(points_new, axis=0)
             jump_1_idx = find_nearest(np.concatenate(points_new, axis=0), np.asarray(pcurr))
             pcurr = points.pop(ind)
             jump_2_idx = find_nearest(np.concatenate(points_new, axis=0), np.asarray(pcurr))
+            print(jump_1_idx, jump_2_idx)
             add_on = points_new[jump_2_idx:jump_1_idx].copy()
             add_on.reverse()
             for ii in add_on:
@@ -75,24 +92,62 @@ def order_points(points, ind):
         pcurr  = points_new[-1]               # update the current point
     return points_new
 
-file_name = 'images/test_draw_2.png'
+file_name = 'images/test_draw_1.png'
 image = cv2.imread(file_name, cv2.IMREAD_GRAYSCALE)
+
+# scale_percent = 50 # percent of original size
+# width = int(image.shape[1] * scale_percent / 100)
+# height = int(image.shape[0] * scale_percent / 100)
+# dim = (width, height)
+# image = cv2.resize(image, dim, interpolation = cv2.INTER_AREA)
+
 _, image = cv2.threshold(image, 128, 255, cv2.THRESH_OTSU + cv2.THRESH_BINARY_INV)
 image = cv2.ximgproc.thinning(image)
 points = cv2.findNonZero(image)
 points = np.squeeze(points)
 ext = get_end_pnts(points, image)
-print(ext[0])
+
+corners = cv2.goodFeaturesToTrack(image, 10,0.005,50)
+corners = np.int0(corners)
+
+for i in corners:
+    points = np.append(points, i, axis = 0)
+    # x, y = i.ravel()
+    # plt.scatter(x,y)
+
 plt.imshow(image, aspect="auto", cmap="gray")
 
 idx = find_nearest(points, ext[0])
-print(idx)
-print(points.shape)
 points_new = order_points(list(points), idx)
 
 x,y  = np.array(points_new).T
-x, y = sampling(x, y)
-for ii in range(len(x)): 
-    plt.scatter(x[ii], y[ii], color = 'blue')
+
+
+x_new = []
+y_new = []
+theta_threshold = math.pi / 10
+for ii in range(len(x)-1):
+
+    if ii > len(x) - 2:
+        break
+    else:
+        is_corner = [x[ii],y[ii]] in corners
+    if is_corner:
+        x_new.append(x[ii])
+        y_new.append(y[ii])
+        continue
+    elif angle(x[ii], y[ii], x[ii+1], y[ii+1]) < theta_threshold:
+        if (ii % 100) == 0:
+            x_new.append(x[ii])
+            y_new.append(y[ii])
+    else:
+        x = np.delete(x, ii + 1)
+        y = np.delete(y, ii + 1)
+        ii = ii - 1
+
+# x_new, y_new = sampling(x, y)
+
+for ii in range(len(x_new)): 
+    plt.scatter(x_new[ii], y_new[ii], color = 'blue')
     plt.pause(0.01)
 plt.show()
