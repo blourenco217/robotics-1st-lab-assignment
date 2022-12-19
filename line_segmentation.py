66f8731c78c0a02e5c2ad7b0821e78117ec2c65e
blourenco217 <beatriz.b.lourenco@tecnico.ulisboa.pt>
2022-12-19 14:12:39 +0000

    somewhat good contours + end points + biforcccss

--- line_segmentation.py
+++ line_segmentation.py
@@ -71,6 +71,11 @@ def count_around_1(image,x,y): #count number of non zero pixels around
 
 def count_around(image,x,y,win): #count number of non zero pixels around certain window
     count = 0
+    w = len(image[0]) #3075 - x
+    l = len(image) #6031 - y
+    
+    # if (x + win) > w or (x - win) < 0 or (y + win) > l or (y - win) < 0: #estava a sair fora das bounds e entao fizemos check
+    #    return 0
     for i in range(-win,win+1): #-1 to 1, -2 to 2
         if i == -win or i == win:
             for j in range(-win,win+1):
@@ -85,9 +90,13 @@ def count_around(image,x,y,win): #count number of non zero pixels around certain
 
 def check_end(image,x,y):
     end_pnt=0
-    gap = range(1,3) #1 or 2
 
-    if count_around(image,x,y,3)==1 or count_around(image,x,y,4)==1 or count_around(image,x,y,5)==1:
+    if count_around(image,x,y,1)==1 or\
+       count_around(image,x,y,2)==1 or\
+       count_around(image,x,y,3)==1 or\
+       count_around(image,x,y,4)==1:# or\
+       #count_around(image,x,y,5)==1:
+        # print(count_around(image,x,y,1))
         end_pnt = 1
 
     return end_pnt
@@ -98,8 +107,16 @@ def check_biforc(image,x,y):
 
     branches = count_around(image,x,y,1)
     if branches > 2:
-        if count_around(image,x,y,2)>=branches and count_around(image,x,y,3)>=branches and  count_around(image,x,y,4)>=branches and count_around(image,x,y,5)>=branches and count_around(image,x,y,6)>=branches and count_around(image,x,y,7)>=branches and count_around(image,x,y,8)>=branches and count_around(image,x,y,9)>=branches and count_around(image,x,y,10)>=branches:
-            biforc=1
+        if  count_around(image,x,y,2)>=branches and \
+            count_around(image,x,y,3)>=branches and \
+            count_around(image,x,y,4)>=branches and \
+            count_around(image,x,y,5)>=branches and \
+            count_around(image,x,y,6)>=branches and \
+            count_around(image,x,y,7)>=branches and \
+            count_around(image,x,y,8)>=branches and \
+            count_around(image,x,y,9)>=branches and \
+            count_around(image,x,y,10)>=branches:
+                biforc=1
                                 
     return biforc
 
@@ -111,6 +128,7 @@ def clustering(points,start, stop): #takes array and saves it in cluster
 
 
 def order_points(points, ind):
+
     points_new = [ points.pop(ind) ]  # initialize a new list of points with the known first point
     pcurr      = points_new[-1]       # initialize the current point (as the known point)
     print(len(points))
@@ -137,6 +155,18 @@ def order_points(points, ind):
         pcurr  = points_new[-1]               # update the current point
     return points_new
 
+
+"""""
+def contour():
+    return contour
+    
+
+
+def path_choice (countors)
+#countors is a an array of countors, which are arrays
+
+"""
+
 file_name = 'images/test_draw_2.png'
 original_image = cv2.imread(file_name, cv2.IMREAD_GRAYSCALE)
 
@@ -145,46 +175,77 @@ original_image = cv2.imread(file_name, cv2.IMREAD_GRAYSCALE)
 # height = int(image.shape[0] * scale_percent / 100)
 # dim = (width, height)
 # image = cv2.resize(image, dim, interpolation = cv2.INTER_AREA)
-
-_, image = cv2.threshold(original_image, 128, 255, cv2.THRESH_OTSU + cv2.THRESH_BINARY_INV)
+image = cv2.copyMakeBorder(original_image, 10, 10, 10, 10, cv2.BORDER_CONSTANT, value = 255)
+_, image = cv2.threshold(image, 128, 255, cv2.THRESH_OTSU + cv2.THRESH_BINARY_INV)
 image = cv2.ximgproc.thinning(image)
 points = cv2.findNonZero(image)
 
+plt.imshow(image,  aspect="auto", cmap="gray")
 
-biforc_pnts=[]
+biforc_pnts = []
 tresh = 5 #window to delete
 for i in points:
-    x, y = i.ravel()
+    x,y = i.ravel()
     if check_biforc(image,x,y):
-        biforc_pnts = np.append(biforc_pnts, i)
-        plt.scatter(x,y, color = 'red')
+        biforc_pnts = np.append(biforc_pnts,i)
+        plt.scatter(x,y,color = 'red')
         for ii in range(-tresh,tresh+1): #erase point and its neighbors
-            for jj in range(-tresh, tresh+1):
-                #points = np.delete(points, i)
-                pass
-
+            for jj in range(-tresh,tresh+1):
+                image[y+jj][x+ii] = 0
+            #points = np.delete(points,ii)
+            pass
+print(biforc_pnts)
 end_pnts=[]
 for i in points:
-    x, y = i.ravel()
+    x,y = i.ravel()
     if check_end(image,x,y):
-        end_pnts = np.append(end_pnts, i)
-        plt.scatter(x,y, color = 'blue')
-        for ii in range(-tresh,tresh+1): #erase point and its neighbors
-            for jj in range(-tresh, tresh+1):
-                #points = np.delete(points, i)
-                pass
+        end_pnts = np.append(end_pnts,i)
+        plt.scatter(x,y,color = 'orange')
 
+contours, _ = cv2.findContours(image, cv2.RETR_LIST,cv2.CHAIN_APPROX_SIMPLE)
 
+plot = False
+i = 0
+for contour in contours:
+    print('coucou')
+    x = [i[:,0] for i in contour]
+    y = [i[:,1] for i in contour]
 
+    if plot:
+        if i == 0:
+            plt.plot(x,y, color='blue')
+        if i == 1:
+            plt.plot(x,y, color='red')
+        if i == 2:
+            plt.plot(x,y, color='green')
+        if i == 3:
+            plt.plot(x,y, color='pink')
+        if i == 4:
+            plt.plot(x,y, color='orange')
+        if i == 5:
+            plt.plot(x,y, color='yellow')
+        i = i+1
 
+    x = x[::100]
+    y = y[::100]
 
-
-
-
-plt.imshow(image)
+    for i in range(len(x)):
+        plt.scatter(x[i], y[i])
+        plt.pause(0.01)
+    plt.pause(1)
 
 #plt.imshow(original_image, aspect="auto", cmap="gray")
 plt.show()
+
+
+
+""""
+countors = [] # ja ordenados a comecar num end point
+for i in countors[]
+"""
+
+
+
 
 
 """"
